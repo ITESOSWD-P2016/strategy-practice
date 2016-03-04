7f151b3212ea01702444e7c80a1493c8742a3d94
Rodrigo Villalobos <rvillalobos@tacitknowledge.com>
2016-03-03 22:35:41 -0600

    first commit

---
+++ src/main/java/com/iteso/nintendo/Mario.java
@@ -0,0 +1,61 @@
+package com.iteso.nintendo;
+
+/**
+ * Created by rvillalobos on 3/3/16.
+ */
+public class Mario extends NintendoCharacter{
+
+    public Mario(){
+        setCurrentPower("normal");
+        setName("Mario");
+        setSpeed("normal");
+    }
+
+    @Override
+    public String performXButtonAction() {
+        return null;
+    }
+
+    @Override
+    public String performYButtonAction() {
+        return null;
+    }
+
+    @Override
+    public String performAButtonAction() {
+        return "jump";
+    }
+
+    @Override
+    public String performBButtonAction() {
+        if (getCurrentPower() == "fire")
+            return "fireball";
+        else if (getCurrentPower() == "invincibility")
+            return "dash";
+        else if (getCurrentPower() == "normal")
+            return "nothing";
+        else
+            return "error";
+    }
+
+    @Override
+    public void setPower(String powerItem) {
+        if (getCurrentPower().toLowerCase() != "normal") {
+            return;
+        }
+
+        else if(powerItem.toLowerCase() == "flower"){
+            setCurrentPower("fire");
+        }
+        else if(powerItem.toLowerCase() == "star"){
+            setCurrentPower("invincibility");
+            setSpeed("fast");
+        }
+        else if(powerItem.toLowerCase() == "clear power"){
+            setCurrentPower("normal");
+            setSpeed("normal");
+        }
+        else
+            setCurrentPower("error");
+    }
+}
---
+++ src/main/java/com/iteso/nintendo/NintendoCharacter.java
@@ -0,0 +1,43 @@
+package com.iteso.nintendo;
+
+/**
+ * Created by rvillalobos on 3/3/16.
+ */
+public abstract class NintendoCharacter {
+      private String speed = null;
+      private String name = null;
+      private String currentPower = null;
+
+
+    public String getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(String speed) {
+        this.speed = speed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public abstract String performXButtonAction();
+    public abstract String performYButtonAction();
+    public abstract String performAButtonAction();
+    public abstract String performBButtonAction();
+
+    public abstract void setPower(String powerItem);
+
+
+    public String getCurrentPower() {
+        return currentPower;
+    }
+
+    public void setCurrentPower(String currentPower) {
+        this.currentPower = currentPower;
+    }
+}
