f6e5b7ca1a6b067205d05958d17f144bf6aeb70e
rvillalobos <rvillalobos@tacitknowledge.com>
2016-03-03 22:48:39 -0600

    Added files via upload

---
+++ src/main/java/com/iteso/nintendo/Luigi.java
@@ -0,0 +1,61 @@
+package com.iteso.nintendo;
+
+/**
+ * Created by rvillalobos on 3/3/16.
+ */
+public class Luigi extends NintendoCharacter{
+
+    public Luigi(){
+        setCurrentPower("normal");
+        setName("Luigi ");
+        setSpeed("normal");
+    }
+
+    @Override
+    public String performXButtonAction() {
+        return null;
+    }
+
+    @Override
+    public String performYButtonAction() {
+        return null;
+    }
+
+    @Override
+    public String performAButtonAction() {
+        return "jump";
+    }
+
+    @Override
+    public String performBButtonAction() {
+        if (getCurrentPower() == "fire")
+            return "fireball";
+        else if (getCurrentPower() == "invincibility")
+            return "dash";
+        else if (getCurrentPower() == "normal")
+            return "nothing";
+        else
+            return "error";
+    }
+
+    @Override
+    public void setPower(String powerItem) {
+        if (getCurrentPower().toLowerCase() != "normal") {
+            return;
+        }
+
+        else if(powerItem.toLowerCase() == "flower"){
+            setCurrentPower("fire");
+        }
+        else if(powerItem.toLowerCase() == "star"){
+            setCurrentPower("invincibility");
+            setSpeed("fast");
+        }
+        else if(powerItem.toLowerCase() == "clear power"){
+            setCurrentPower("normal");
+            setSpeed("normal");
+        }
+        else
+            setCurrentPower("error");
+    }
+}
